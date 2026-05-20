357c745a834926536111ee657be4ec3a14906288
xinjiaantian83-prog <xinjiaantian83-prog@users.noreply.github.com>
2026-05-20 13:12:47 +0900

    Use CartoDB Positron map tiles

--- js/app.js
+++ js/app.js
@@ -43,10 +43,10 @@
     tap: true,
   });
 
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
     maxZoom: 19,
     attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
   }).addTo(map);
 
   // 地図クリック時は詳細カードを閉じる
